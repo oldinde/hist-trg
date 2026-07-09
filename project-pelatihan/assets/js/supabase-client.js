cd11b4caa3c8f136b81420d0a65bb3d62adf78fb
oldinde <kppindeotd@gmail.com>
2026-07-09 10:29:35 +0800

    Update supabase-client.js

--- project-pelatihan/assets/js/supabase-client.js
+++ project-pelatihan/assets/js/supabase-client.js
@@ -3,4 +3,4 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = 'https://uarvahbizfvltltwdhzb.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_LxdEDCcdffWp0nxzKQlF6Q_Jd7ucAiG';
 
-window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
