9c1b6d296ec40469a8988ea089c5372a8ed4a930
oldinde <kppindeotd@gmail.com>
2026-07-09 10:34:12 +0800

    Update supabase.js

---
+++ project-pelatihan/config/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://uarvahbizfvltltwdhzb.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_LxdEDCcdffWp0nxzKQlF6Q_Jd7ucAiG';
+
+const supabase = window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
